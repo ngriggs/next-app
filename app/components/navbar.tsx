8140c929e85bffd25edf917e3fd00435050dbf40
Noah Griggs <noahg@vt.edu>
2023-02-05 13:07:16 -0600

    navbar updates
    changed navbar to always be on top and changed top right button to book now

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -148,176 +148,177 @@ class Navbar extends Component<Props, State> {
 
 	render() {
 		return (
-			<Headroom
-				style={{
-					zIndex: 40,
-					maxWidth: "1280px",
-					marginLeft: "auto",
-					marginRight: "auto",
-				}}
-				className=""
-			>
-				<Popover as="nav" className="relative bg-white ">
-					<div className="mx-auto px-1 sm:px-0 ">
-						<div className="flex  max-h-[80px] items-center justify-between border-b-2 border-gray-100 py-3 md:justify-start md:space-x-10 px-0">
-							<div className="flex justify-start lg:w-0 lg:flex-1">
-								<nav>
-									<Link
-										href="/"
-										className="flex justify-center items-center hover:scale-105"
-									>
-										<Image
-											width={144}
-											height={10}
-											alt="logo"
-											className="object-cover object-center w-32 h-20"
-											src="giggles.svg"
-											priority={true}
-										/>
-									</Link>
-								</nav>
-							</div>
-							<div className="-my-2 -mr-2 md:hidden">
-								<Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 mr-5 text-gray-400 hover:bg-gray-100 hover:text-black focus:outline-none focus:ring-2 focus:ring-inset">
-									<span className="sr-only">Open menu</span>
-									<Bars3Icon className="h-6 w-6" aria-hidden="true" />
-								</Popover.Button>
-							</div>
-
-							<Popover.Group as="nav" className="hidden space-x-8 md:flex ">
+			// <Headroom
+			// 	style={{
+			// 		zIndex: 40,
+			// 		maxWidth: "1280px",
+			// 		marginLeft: "auto",
+			// 		marginRight: "auto",
+			// 	}}
+			// 	className=""
+			// >
+			<Popover as="nav" className="sticky top-0 z-50 bg-white ">
+				<div className="mx-auto px-1 sm:px-0 ">
+					<div className="flex  max-h-[80px] items-center justify-between border-b-2 border-gray-100 py-3 md:justify-start md:space-x-10 px-0">
+						<div className="flex justify-start lg:w-0 lg:flex-1">
+							<nav>
 								<Link
 									href="/"
-									className="text-base font-semibold text-gray-500 hover:text-black hover:underline"
+									className="flex justify-center items-center hover:scale-105"
 								>
-									home
-								</Link>
-								<PopoverMenu solutions={visit} title="visit" />
-								<PopoverMenu solutions={party} title="party" />
-								<Link
-									href="/about"
-									className="text-base font-semibold text-gray-500 hover:text-black hover:underline"
-								>
-									learn
-								</Link>
-								<PopoverMenu solutions={read} title="read" />
-							</Popover.Group>
-							<div className="hidden items-center justify-end md:flex md:flex-1 lg:w-0">
-								<Link
-									href="/contact"
-									className="ml-8 mr-4 inline-flex items-center justify-center whitespace-nowrapborder border-transparent  px-4 py-2 text-base"
-								>
-									<Button
-										label="get in touch"
-										bgColor="[#ec6a52]"
-										hoverColor=""
-										styles="shadow-sm font-medium "
+									<Image
+										width={144}
+										height={10}
+										alt="logo"
+										className="object-cover object-center w-32 h-20"
+										src="giggles.svg"
+										priority={true}
 									/>
 								</Link>
-							</div>
+							</nav>
+						</div>
+						<div className="-my-2 -mr-2 md:hidden">
+							<Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 mr-5 text-gray-400 hover:bg-gray-100 hover:text-black focus:outline-none focus:ring-2 focus:ring-inset">
+								<span className="sr-only">Open menu</span>
+								<Bars3Icon className="h-6 w-6" aria-hidden="true" />
+							</Popover.Button>
+						</div>
+
+						<Popover.Group as="nav" className="hidden space-x-8 md:flex ">
+							<Link
+								href="/"
+								className="text-base font-semibold text-gray-500 hover:text-black hover:underline"
+							>
+								home
+							</Link>
+							<PopoverMenu solutions={visit} title="visit" />
+							<PopoverMenu solutions={party} title="party" />
+							<Link
+								href="/about"
+								className="text-base font-semibold text-gray-500 hover:text-black hover:underline"
+							>
+								learn
+							</Link>
+							<PopoverMenu solutions={read} title="read" />
+						</Popover.Group>
+						<div className="hidden items-center justify-end md:flex md:flex-1 lg:w-0">
+							<Link
+								href="https://squareup.com/appointments/book/89lmsk6jh008w1/LPJGJT0WFHK20/start"
+								target={"_blank"}
+								className="ml-8 mr-4 inline-flex items-center justify-center whitespace-nowrapborder border-transparent  px-4 py-2 text-base"
+							>
+								<Button
+									label="Book Now"
+									bgColor="[#ec6a52]"
+									hoverColor=""
+									styles="shadow-sm font-medium"
+								/>
+							</Link>
 						</div>
 					</div>
+				</div>
 
-					<Transition
-						as={Fragment}
-						enter="duration-100 ease-out"
-						enterFrom="opacity-0 scale-95"
-						enterTo="opacity-100 scale-100"
-						leave="duration-100 ease-in"
-						leaveFrom="opacity-100 scale-100"
-						leaveTo="opacity-0 scale-95"
+				<Transition
+					as={Fragment}
+					enter="duration-100 ease-out"
+					enterFrom="opacity-0 scale-95"
+					enterTo="opacity-100 scale-100"
+					leave="duration-100 ease-in"
+					leaveFrom="opacity-100 scale-100"
+					leaveTo="opacity-0 scale-95"
+				>
+					<Popover.Panel
+						focus
+						className="fixed w-full top-0 transform transition md:hidden"
 					>
-						<Popover.Panel
-							focus
-							className="fixed w-full top-0 transform transition md:hidden"
-						>
-							{({ close }) => (
-								<button
-									className="absolute w-full text-left p-2"
-									onClick={async () => {
-										await close();
-									}}
-								>
-									<div className="divide-y-2 divide-gray-50 rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
-										<div className="px-5 pt-2 pb-6">
-											<div className="flex items-center justify-between">
-												<div>
-													<Link href="/">
-														<img
-															className="w-auto h-12"
-															src="giggles-logo-2.svg"
-														/>
-													</Link>
-												</div>
-												<div className="-mr-2">
-													<div className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-black focus:outline-none focus:ring-2 focus:ring-inset">
-														<span className="sr-only">Close menu</span>
-														<XMarkIcon className="h-6 w-6" aria-hidden="true" />
-													</div>
-												</div>
+						{({ close }) => (
+							<button
+								className="absolute w-full text-left p-2"
+								onClick={async () => {
+									await close();
+								}}
+							>
+								<div className="divide-y-2 divide-gray-50 rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
+									<div className="px-5 pt-2 pb-6">
+										<div className="flex items-center justify-between">
+											<div>
+												<Link href="/">
+													<img
+														className="w-auto h-12"
+														src="giggles-logo-2.svg"
+													/>
+												</Link>
 											</div>
-											<div className="mt-6">
-												<nav className="grid gap-y-8">
-													{mobile1.map((item) => (
-														<Link
-															key={item.name}
-															href={item.href}
-															target={item.target}
-															className="-m-3 flex items-center rounded-md p-3 hover:bg-gray-50"
-														>
-															<item.icon
-																className="h-6 w-6 flex-shrink-0 text-[#435a6c]"
-																aria-hidden="true"
-															/>
-															<span className="ml-3 text-base font-medium text-gray-900">
-																{item.name}
-															</span>
-														</Link>
-													))}
-												</nav>
+											<div className="-mr-2">
+												<div className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-black focus:outline-none focus:ring-2 focus:ring-inset">
+													<span className="sr-only">Close menu</span>
+													<XMarkIcon className="h-6 w-6" aria-hidden="true" />
+												</div>
 											</div>
 										</div>
-										<div className="space-y-6 py-6 px-5">
-											<div className="grid grid-cols-2 gap-y-4 gap-x-8">
-												<Link
-													href="/articles"
-													className="text-base font-medium text-gray-900 hover:text-gray-700"
-												>
-													blog
-												</Link>
-
-												<Link
-													href="/learn"
-													className="text-base font-medium text-gray-900 hover:text-gray-700"
-												>
-													learn
-												</Link>
-												{mobile2.map((item) => (
+										<div className="mt-6">
+											<nav className="grid gap-y-8">
+												{mobile1.map((item) => (
 													<Link
 														key={item.name}
 														href={item.href}
 														target={item.target}
-														className="text-base font-medium text-gray-900 hover:text-gray-700"
+														className="-m-3 flex items-center rounded-md p-3 hover:bg-gray-50"
 													>
-														{item.name}
+														<item.icon
+															className="h-6 w-6 flex-shrink-0 text-[#435a6c]"
+															aria-hidden="true"
+														/>
+														<span className="ml-3 text-base font-medium text-gray-900">
+															{item.name}
+														</span>
 													</Link>
 												))}
-											</div>
-											<div>
-												<Link
-													href="/contact"
-													className="flex w-full items-center justify-center rounded-md border border-transparent bg-[#99bdbb] px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-[#435a6c]"
-												>
-													get in touch
-												</Link>
-											</div>
+											</nav>
 										</div>
 									</div>
-								</button>
-							)}
-						</Popover.Panel>
-					</Transition>
-				</Popover>
-			</Headroom>
+									<div className="space-y-6 py-6 px-5">
+										<div className="grid grid-cols-2 gap-y-4 gap-x-8">
+											<Link
+												href="/articles"
+												className="text-base font-medium text-gray-900 hover:text-gray-700"
+											>
+												blog
+											</Link>
+
+											<Link
+												href="/learn"
+												className="text-base font-medium text-gray-900 hover:text-gray-700"
+											>
+												learn
+											</Link>
+											{mobile2.map((item) => (
+												<Link
+													key={item.name}
+													href={item.href}
+													target={item.target}
+													className="text-base font-medium text-gray-900 hover:text-gray-700"
+												>
+													{item.name}
+												</Link>
+											))}
+										</div>
+										<div>
+											<Link
+												href="/contact"
+												className="flex w-full items-center justify-center rounded-md border border-transparent bg-[#99bdbb] px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-[#435a6c]"
+											>
+												get in touch
+											</Link>
+										</div>
+									</div>
+								</div>
+							</button>
+						)}
+					</Popover.Panel>
+				</Transition>
+			</Popover>
+			// </Headroom>
 		);
 	}
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,30 +2,74 @@ import Button from "./components/button";
 import Carousel from "./components/carousel";
 import HeroPage from "./components/hero";
 import Gallery2 from "./gallery/page";
-import { gql, datocms } from "../lib/datocms";
+import { datocms } from "../lib/datocms";
 import Link from "next/link";
 
-// const GetAllPostsQuery = gql`
-// 	query getAllArticles {
-// 		allArticles {
-// 			id
-// 			title
-// 			_status
-// 			content {
-// 				blocks
-// 				links
-// 				value
+// const getUserDetailByFetchAPICall = async () => {
+// 	try {
+// 	  const headers = {
+// 		'content-type': 'application/json',
+// 		'Authorization': `Bearer ${process.env.DATOCMS_READONLY_TOKEN}`
+// 	  };
+// 	  const requestBody = {
+// 		query: `
+// 		query getAllArticles {
+// 			allArticles {
+// 				id
+// 				title
+// 				_status
+// 				content {
+// 					blocks
+// 					links
+// 					value
+// 				}
+// 				slug
+// 				_createdAt
 // 			}
-// 			slug
-// 			_createdAt
+// 			_allArticlesMeta {
+// 				count
+// 			}
 // 		}
-// 		_allArticlesMeta {
-// 			count
-// 		}
+// 	`
+// 	  };
+// 	  const options = {
+// 		method: 'GET',
+// 		headers,
+// 		body: JSON.stringify(requestBody)
+// 	  };
+// 	  const response = await (await fetch(`${process.env.DATOCMS_API_URL}`, options)).json();
+// 	  console.log('RESPONSE FROM FETCH REQUEST', response?.data);
+// 	  setUserDetails(response?.data?.nextUser ?? {});
 // 	}
-// `;
+// 	catch (err) {
+// 	  console.log('ERROR DURING FETCH REQUEST', err);
+// 	}
+// 	finally {
+// 	  setIsLoading(false);
+// 	}
+//   };
 
 export default async function HomePage() {
+	const GetAllPostsQuery = `
+		query getAllArticles {
+			allArticles {
+				id
+				title
+				_status
+				content {
+					blocks
+					links
+					value
+				}
+				slug
+				_createdAt
+			}
+			_allArticlesMeta {
+				count
+			}
+		}
+	`;
+
 	// const { postCollection } = await datocms.request(GetAllPostsQuery, {
 	// 	first: 10,
 	// });
