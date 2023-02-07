105b319b0927caf2c4ce33cae68ce57c540a7448
Noah Griggs <noahg@vt.edu>
2023-02-06 21:24:13 -0600

    Update head.tsx

--- app/head.tsx
+++ app/head.tsx
@@ -2,12 +2,12 @@ export default function Head() {
 	return (
 		<>
 			<title>Giggles Play</title>
+			<link rel="icon" href="/favicon.ico" />
 			<meta
 				content="width=device-width, initial-scale=1"
 				name="viewport"
 				key="title"
 			/>
-			<link rel="icon" href="/favicon.ico" />
 		</>
 	);
 }
