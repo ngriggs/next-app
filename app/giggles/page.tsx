042320f0f2380440b0e5cbe22443399db410ebd8
Noah Griggs <noahg@vt.edu>
2023-02-11 14:08:58 -0600

    Update page.tsx
    changed margins and paragraph font

--- app/giggles/page.tsx
+++ app/giggles/page.tsx
@@ -1,10 +1,10 @@
 export default function Giggles() {
 	return (
-		<div className="flex flex-col items-center justify-center p-10">
+		<div className="flex flex-col items-center justify-center p-4">
 			<div className="container mx-auto p-4">
 				<h1 className="font-summerVibesSolid font-normal text-5xl mb-4 text-center">about giggles</h1>
 
-				<p className="mb-4">
+				<p className="font-quicksand mb-4">
 					At Giggles Play, we believe that play and learning go hand-in-hand.
 					Our mission is to provide a dynamic and welcoming environment where
 					families can form meaningful connections through play. We offer a
@@ -15,14 +15,14 @@ export default function Giggles() {
 				</p>
 
 				<h2 className="font-summerVibesSolid font-normal text-3xl mb-2">our vision</h2>
-				<p className="mb-4">
+				<p className="font-quicksand mb-4">
 					To be the premier destination for families seeking a safe, nurturing,
 					and engaging environment where they can connect with each other
 					through play and learning.
 				</p>
 
 				<h2 className="font-summerVibesSolid font-normal text-3xl mb-2">mission statement</h2>
-				<p className="mb-4">
+				<p className="font-quicksand mb-4">
 					To provide a dynamic and welcoming environment where families can form
 					meaningful connections through play, foster children's growth and
 					encourage the development of important life skills, and foster a
@@ -31,14 +31,14 @@ export default function Giggles() {
 				</p>
 
 				<h2 className="font-summerVibesSolid font-normal text-3xl mb-2">facilities + programs</h2>
-				<p className="mb-4">
+				<p className="font-quicksand mb-4">
 					In this section, you can include details about your play areas,
 					activities, and classes. You can also highlight what makes Giggles
 					Play unique and different from other playspaces.
 				</p>
 
 				<h2 className="font-summerVibesSolid font-normal text-3xl mb-2">why choose giggles</h2>
-				<p className="mb-4">
+				<p className="font-quicksand mb-4">
 					In this section, you can highlight the benefits of choosing Giggles
 					Play as the destination for families seeking an environment that
 					promotes play and learning. You can include testimonials from
@@ -47,7 +47,7 @@ export default function Giggles() {
 				</p>
 
 				<h2 className="font-summerVibesSolid font-normal text-3xl mb-2">contact us</h2>
-				<p className="mb-4">
+				<p className="font-quicksand mb-4">
 					Include details about how families can get in touch with you, such as
 					your address, phone number, email, and any social media handles.
 				</p>
