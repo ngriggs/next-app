d542dae543d7576419883792d2fc082298e2aa1a
Noah Griggs <noahg@vt.edu>
2023-02-11 11:04:17 -0600

    Create page.tsx

---
+++ app/giggles/page.tsx
@@ -0,0 +1,15 @@
+export default function Giggles() {
+	return (
+		<div>
+			<p>
+				At Giggles Play, our mission is to provide a dynamic and welcoming
+				environment where parents and caregivers can form meaningful connections
+				with their children through play. Our clean and inviting facility offers
+				thoughtfully designed play areas, activities, and classes that foster
+				independence, creativity, and confidence in children. We strive to be a
+				hub where families can build authentic relationships and create lifelong
+				friendships with one another.
+			</p>
+		</div>
+	);
+}
