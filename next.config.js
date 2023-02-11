a010e130f5ed3b515dcce055a919c9b1127cd1d3
Noah Griggs <noahg@vt.edu>
2023-02-11 09:53:01 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -16,7 +16,7 @@ const nextConfig = {
 				protocol: "https",
 				hostname: "scontent-ord5-2.cdninstagram.com",
 				port: "",
-				pathname: "/v/t51.29350-15/**",
+				pathname: "/**",
 			},
 		],
 	},
