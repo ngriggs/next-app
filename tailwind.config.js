9238f24b3eb3fb790a8989747a0039f0a943aae5
Noah Griggs <noahg@vt.edu>
2023-02-19 14:08:53 -0600

    jess updates
    -changed tagline in footer
    -added giggles colors to config.js
    -added copy to landing page
    -changed section headers on about giggles page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,47 +1,54 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	darkMode: ["class", '[data-theme="dark"]'],
-	content: [
-		"./app/**/*.{js,ts,jsx,tsx}",
-		"./pages/**/*.{js,ts,jsx,tsx}",
-		"./components/**/*.{js,ts,jsx,tsx}",
+  darkMode: ["class", '[data-theme="dark"]'],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
 
-		// Or if using `src` directory:
-		"./src/**/*.{js,ts,jsx,tsx}",
-	],
-	theme: {
-		extend: {
-			fontFamily: {
-				poppins: ["Poppins", "sans-serif"],
-				summerVibesBold: ["SummerVibesBold", "sans-serif"],
-				summerVibesLight: ["SummerVibesLight"],
-				summerVibesRegular: ["SummerVibesRegular"],
-				summerVibesSolid: ["SummerVibesSolid"],
-				quicksand: ["Quicksand"],
-			},
-			keyframes: {
-				"accordion-down": {
-					from: { height: 0 },
-					to: { height: "var(--radix-accordion-content-height)" },
-				},
-				"accordion-up": {
-					from: { height: "var(--radix-accordion-content-height)" },
-					to: { height: 0 },
-				},
-			},
-			animation: {
-				"accordion-down": "accordion-down 0.2s ease-out",
-				"accordion-up": "accordion-up 0.2s ease-out",
-			},
-			gridTemplateColumns: {
-				fluid: "repeat(auto-fit, minmax(15rem, 1fr))",
-			},
-		},
-	},
-	plugins: [
-		require("@tailwindcss/line-clamp"),
-		require("@tailwindcss/typography"),
-		require("@headlessui/tailwindcss"),
-		require("tailwindcss-animate"),
-	],
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        summerVibesBold: ["SummerVibesBold", "sans-serif"],
+        summerVibesLight: ["SummerVibesLight"],
+        summerVibesRegular: ["SummerVibesRegular"],
+        summerVibesSolid: ["SummerVibesSolid"],
+        quicksand: ["Quicksand"],
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+      gridTemplateColumns: {
+        fluid: "repeat(auto-fit, minmax(15rem, 1fr))",
+      },
+      colors: {
+        "giggles-red": "#ec6a52",
+        "giggles-yellow": "#f7ae47",
+        "giggles-blue": "#99bdbb",
+        "giggles-green": "#bbb45a",
+        "giggles-dark-blue": "#435a6c",
+      },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/line-clamp"),
+    require("@tailwindcss/typography"),
+    require("@headlessui/tailwindcss"),
+    require("tailwindcss-animate"),
+  ],
 };
