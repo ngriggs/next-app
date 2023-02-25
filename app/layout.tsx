75ccba85a993d5ce778584b999f7967b0378ff79
Noah Griggs <noahg@vt.edu>
2023-02-24 23:09:17 -0600

    changes to seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,16 @@
 import "./globals.css";
 import Footer from "./components/footer";
-import Navbar from "./components/navbar";
-import Footer2 from "./components/footer2";
 import { MainNav } from "./components/mainNav";
 import { siteConfig } from "@/app/config/site";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "Giggles Play",
+	description: "Welcome to Giggles",
+	icons: {
+		icon: "/sun.svg",
+	},
+};
 
 export default function RootLayout({
 	children,
@@ -12,13 +19,9 @@ export default function RootLayout({
 }) {
 	return (
 		<html>
-			<head />
 			<body className="font-quicksand w-[100%] mx-auto overflow-x-clip overscroll-contain">
 				<div className="max-w-7xl mx-auto font-medium">
-					{/* <Navbar /> */}
-					<div className="my-20" />
 					<MainNav items={siteConfig.mainNav} />
-					{/* <MobileNav /> */}
 					<div className="text-[#232323] mt-[80px]">{children}</div>
 				</div>
 				<div className="">
