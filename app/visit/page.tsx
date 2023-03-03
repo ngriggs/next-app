205542e0f4e8cd90c9cdf24a9934d37d23da7be5
Jessica Ortega <jessicabortega@gmail.com>
2023-03-03 07:03:21 -0600

    Added price to the basic admission list

--- app/visit/page.tsx
+++ app/visit/page.tsx
@@ -36,45 +36,71 @@ const Visit = () => {
         <Button variant={"giggles"}>Sign Waiver</Button>
       </Link>{" "}
       <br />
-      <div className="w-screen bg-[#FEF7EC] py-10 text-center">
-        <h1 className="font-summerVibesSolid text-5xl font-normal">
-          basic admission
-        </h1>
-        <br />
-        <span className="text-gray-600">
-          Adult supervision is required at all times. Please read about our
-          rules and policies.
-        </span>
-        <div className="m-4 mx-auto flex h-16 w-10/12 rounded-full bg-white">
+      <div className="w-screen bg-[#FEF7EC] py-10">
+        <p className="text-center">
+          <span className="font-summerVibesSolid text-5xl font-normal">
+            basic admission
+          </span>
+          <br />
+          <span className="text-gray-600">
+            Adult supervision is required at all times. Please read about our
+            rules and policies.
+          </span>
+        </p>
+        <div className="m-4 mx-auto flex h-16 w-10/12 max-w-3xl grid-cols-3 rounded-full bg-white shadow-md">
           <div className="h-16 w-16 rounded-full bg-giggles-blue">
             <Icons.infantIcon className="mx-auto my-auto mt-2.5 h-10 w-10 fill-white object-[24px]" />
-          </div>{" "}
-          <p>
-            <span className="mt-1 text-lg font-bold">Infant*</span>
-            <br />
-            <span className="ml-2 text-slate-500">6 months and younger</span>
-          </p>
+          </div>
+          <div className="mt-2 leading-3">
+            <p>
+              <span className="ml-2 mt-1 text-lg font-bold">Infant</span> <br />
+              <span className="ml-2 text-slate-500">6 months and younger</span>
+            </p>
+          </div>
+          <div className="mx-auto my-auto h-12 w-16 justify-end rounded-3xl bg-giggles-blue pt-2 text-center text-xl text-white">
+            free
+          </div>
           <br />
         </div>
-        <div className="m-4 mx-auto flex h-16 w-10/12 rounded-full bg-white shadow-lg">
+        <div className="m-4 mx-auto flex w-10/12 max-w-3xl grid-cols-3 rounded-full bg-white shadow-md sm:h-16">
           <div className="h-16 w-16 rounded-full bg-giggles-blue">
             <Icons.infantIcon className="mx-auto my-auto mt-2.5 h-10 w-10 fill-white object-[24px]" />
-          </div>{" "}
-          <p className="ml-2 mt-1 text-lg font-bold">Child</p>
+          </div>
+          <div className="mt-2 leading-3">
+            <p>
+              <span className="ml-2 mt-1 text-lg font-bold">Child</span> <br />
+              <span className="ml-2 text-slate-500">6 months and older</span>
+            </p>
+          </div>
         </div>
-        <div className="m-4 mx-auto flex h-16 w-10/12 rounded-full bg-white shadow-lg">
+        <div className="m-4 mx-auto flex h-16 w-10/12 max-w-3xl grid-cols-3 rounded-full bg-white shadow-md">
           <div className="h-16 w-16 rounded-full bg-giggles-blue">
             <Icons.infantIcon className="mx-auto my-auto mt-2.5 h-10 w-10 fill-white object-[24px]" />
-          </div>{" "}
-          <p className="ml-2 mt-1 text-lg font-bold">Sibling</p>
+          </div>
+          <div className="mt-2 leading-3">
+            <p>
+              <span className="ml-2 mt-1 text-lg font-bold">Sibling</span>{" "}
+              <br />
+              <span className="ml-2 text-slate-500">
+                rate applies after one paid child
+              </span>
+            </p>
+          </div>
         </div>
-        <div className="m-4 mx-auto flex h-16 w-10/12 rounded-full bg-white shadow-lg">
+        <div className="m-4 mx-auto flex h-16 w-10/12 max-w-3xl grid-cols-3 rounded-full bg-white shadow-md">
           <div className="h-16 w-16 rounded-full bg-giggles-blue">
             <Icons.infantIcon className="mx-auto my-auto mt-2.5 h-10 w-10 fill-white object-[24px]" />
-          </div>{" "}
-          <p className="ml-2 mt-1 text-lg font-bold">
-            Adult without paid child
-          </p>
+          </div>
+          <div className="mt-2 leading-3">
+            <p>
+              {" "}
+              <span className="ml-2 mt-1 text-lg font-bold">
+                Adult without a paid child
+              </span>{" "}
+              <br />
+              <span className="ml-2 text-slate-500">age 13 and older</span>
+            </p>
+          </div>
         </div>
       </div>
     </div>
