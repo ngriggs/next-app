2f42a06f5a08e4ec8675847476a8b8ab9e08040a
Jessica Ortega <jessicabortega@gmail.com>
2023-03-04 09:24:55 -0600

    max number of instagram posts on home page

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -3,53 +3,53 @@ import Loading from "../loading";
 import InstagramCard from "../components/instagramCard";
 
 interface Image {
-	id: string;
-	caption: string;
-	media_url: string;
-	timestamp: string;
-	media_type: string;
-	permalink: string;
+  id: string;
+  caption: string;
+  media_url: string;
+  timestamp: string;
+  media_type: string;
+  permalink: string;
 }
 
 interface ResponseData {
-	data: Image[];
+  data: Image[];
 }
 
 async function getImages(): Promise<ResponseData> {
-	const url = `https://graph.instagram.com/me/media?fields=id,caption,media_url,timestamp,media_type,permalink&access_token=${process.env.INSTAGRAM_KEY}`;
-	const data = await fetch(url);
-	if (!data.ok) {
-		throw new Error("Failed to fetch data");
-	}
-	return data.json();
+  const url = `https://graph.instagram.com/me/media?fields=id,caption,media_url,timestamp,media_type,permalink&access_token=${process.env.INSTAGRAM_KEY}`;
+  const data = await fetch(url);
+  if (!data.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  return data.json();
 }
 
-export default async function Gallery2() {
-	const data = await getImages();
+export default async function Gallery2({ ...props }) {
+  const data = await getImages();
 
-	return (
-		<Suspense fallback={<Loading />}>
-			<div className="px-4 py-20">
-				<h1 className="text-center font-summerVibesSolid text-2xl font-normal min-[375px]:text-3xl sm:text-4xl lg:text-5xl ">
-					giggles on the gram
-				</h1>
-				<p className="mx-auto mt-2 max-w-2xl text-center sm:text-xl">
-					Check out our latest Instagram posts! Share your moments at Giggles
-					with #gigglesplay or tag @giggles.play for a chance to be seen here!
-				</p>
-				<div className="mx-auto mt-10 mb-5 grid max-w-7xl grid-cols-1 gap-5 px-6 sm:gap-4 md:grid-cols-2 xl:grid-cols-4 ">
-					{data?.data.map((image: Image, index) => (
-						<InstagramCard
-							key={image.id}
-							id={index}
-							username={image.id}
-							profileImageUrl={image.media_url}
-							postImageUrl={image.permalink}
-							caption={image.caption}
-						/>
-					))}
-				</div>
-			</div>
-		</Suspense>
-	);
+  return (
+    <Suspense fallback={<Loading />}>
+      <div className="px-4 py-20">
+        <h1 className="text-center font-summerVibesSolid text-2xl font-normal min-[375px]:text-3xl sm:text-4xl lg:text-5xl ">
+          giggles on the gram
+        </h1>
+        <p className="mx-auto mt-2 max-w-2xl text-center text-slate-600 sm:text-xl">
+          Check out our latest Instagram posts! Share your moments at Giggles
+          with #gigglesplay or tag @giggles.play for a chance to be seen here!
+        </p>
+        <div className="mx-auto mt-10 mb-5 grid max-w-7xl grid-cols-1 gap-5 px-6 sm:gap-4 md:grid-cols-2 xl:grid-cols-4 ">
+          {data?.data.slice(0, props.slice).map((image: Image, index) => (
+            <InstagramCard
+              key={image.id}
+              id={index}
+              username={image.id}
+              profileImageUrl={image.media_url}
+              postImageUrl={image.permalink}
+              caption={image.caption}
+            />
+          ))}
+        </div>
+      </div>
+    </Suspense>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -6,49 +6,49 @@ import StayConnected from "./components/stayConnected";
 import Loading from "./loading";
 
 export default async function HomePage() {
-	const images = [
-		{
-			id: 1,
-			url: "moodboard.min.svg",
-			title: "Education re-invented",
-			description:
-				"Teaching kids how to think for themselves and solve meaningful problems",
-		},
-		{
-			id: 2,
-			url: "card2.min.svg",
-			title: "Education re-invented",
-			description:
-				"Teaching kids how to think for themselves and solve meaningful problems",
-		},
-		{
-			id: 3,
-			url: "card3.min.svg",
-			title: "Education re-invented",
-			description:
-				"Teaching kids how to think for themselves and solve meaningful problems",
-		},
-		{
-			id: 4,
-			url: "card4.min.svg",
-			title: "Education re-invented",
-			description:
-				"Teaching kids how to think for themselves and solve meaningful problems",
-		},
-	];
-	return (
-		<div className="mx-auto max-w-7xl text-[#232323]">
-			<div className="grid place-content-center lg:h-[500px] 2xl:h-[900px]">
-				<Carousel images={images} />
-			</div>
-			<div className="sm:my-20" />
-			<HeroPage />
-			<Hero1 />
-			<div className="my-[100px] sm:my-[100px]" />
-			<StayConnected />
-			<div className="sm:my-8" />
-			{/* @ts-expect-error Server Component */}
-			<Gallery2 />
-		</div>
-	);
+  const images = [
+    {
+      id: 1,
+      url: "moodboard.min.svg",
+      title: "Education re-invented",
+      description:
+        "Teaching kids how to think for themselves and solve meaningful problems",
+    },
+    {
+      id: 2,
+      url: "card2.min.svg",
+      title: "Education re-invented",
+      description:
+        "Teaching kids how to think for themselves and solve meaningful problems",
+    },
+    {
+      id: 3,
+      url: "card3.min.svg",
+      title: "Education re-invented",
+      description:
+        "Teaching kids how to think for themselves and solve meaningful problems",
+    },
+    {
+      id: 4,
+      url: "card4.min.svg",
+      title: "Education re-invented",
+      description:
+        "Teaching kids how to think for themselves and solve meaningful problems",
+    },
+  ];
+  return (
+    <div className="mx-auto max-w-7xl text-[#232323]">
+      <div className="grid place-content-center lg:h-[500px] 2xl:h-[900px]">
+        <Carousel images={images} />
+      </div>
+      <div className="sm:my-20" />
+      <HeroPage />
+      <Hero1 />
+      <div className="my-[100px] sm:my-[100px]" />
+      <StayConnected />
+      <div className="sm:my-8" />
+      {/* @ts-expect-error Server Component */}
+      <Gallery2 slice={4} />
+    </div>
+  );
 }
