d00d1ae39793e6e3efc7f2af09cfd0116d1a05bd
Jessica Ortega <jessicabortega@gmail.com>
2023-03-03 23:06:15 -0600

    added slider for pricing

--- app/components/mainNav.tsx
+++ app/components/mainNav.tsx
@@ -8,133 +8,128 @@ import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
 import { MainNavProps } from "../types/nav";
 import Image from "next/image";
 import {
-	Accordion,
-	AccordionContent,
-	AccordionItem,
-	AccordionTrigger,
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
 } from "@/app/components/ui/accordion";
 import { socialMedia } from "../config/site";
-import {
-	HoverCard,
-	HoverCardContent,
-	HoverCardTrigger,
-} from "@/app/components/ui/hoverCard";
 
 export function MainNav({ items }: MainNavProps) {
-	return (
-		<div className="fixed left-[calc(-50vw_+_50%)] top-0 z-50 flex h-[80px] w-screen gap-6 bg-white font-bold md:gap-0">
-			<div className="mx-auto flex max-w-7xl flex-1 px-1 sm:px-0">
-				<div className="my-auto sm:flex sm:flex-1">
-					<Link href="/" className="hover:cursor-pointer">
-						<Image
-							width={400}
-							height={200}
-							alt="logo"
-							className="ml-[-80px] h-11 hover:translate-y-[-1px] md:ml-0 lg:ml-[-80px]"
-							src="/giggles_horizontal_logo.svg"
-							priority={true}
-						/>
-					</Link>
-				</div>
+  return (
+    <div className="fixed left-[calc(-50vw_+_50%)] top-0 z-50 flex h-[80px] w-screen gap-6 bg-white md:gap-0">
+      <div className="mx-auto flex max-w-7xl flex-1 px-1 sm:px-0">
+        <div className="my-auto grid justify-items-start sm:flex sm:flex-1">
+          <Link href="/" className="hover:cursor-pointer">
+            <Image
+              width={200}
+              height={200}
+              alt="logo"
+              className=" h-12 hover:translate-y-[-1px] md:ml-0 "
+              src="/giggles_horizontal_logo.svg"
+              priority={true}
+            />
+          </Link>
+        </div>
 
-				{items?.length ? (
-					<nav className="my-auto hidden justify-center space-x-8 md:flex md:flex-1 ">
-						{items?.map((item, index) => (
-							<Popover key={index}>
-								<PopoverTrigger className="group flex cursor-pointer flex-row text-[#232323] hover:underline hover:underline-offset-[8px]">
-									{item.title}
-									<Icons.chevronDown
-										className="relative my-auto ml-2 h-5 w-5 stroke-1 transition duration-200 group-data-[state=open]:rotate-180"
-										aria-hidden="true"
-									/>
-								</PopoverTrigger>
+        {items?.length ? (
+          <nav className="my-auto hidden justify-center space-x-8 md:flex md:flex-1 ">
+            {items?.map((item, index) => (
+              <Popover key={index}>
+                <PopoverTrigger className="group flex cursor-pointer flex-row text-lg font-medium text-[#232323] hover:underline hover:underline-offset-[8px]">
+                  {item.title}
+                  <Icons.chevronDown
+                    className="relative my-auto ml-2 h-5 w-5 stroke-1 transition duration-200 group-data-[state=open]:rotate-180"
+                    aria-hidden="true"
+                  />
+                </PopoverTrigger>
 
-								<PopoverContent
-									side="bottom"
-									align="start"
-									alignOffset={-20}
-									className="shrink text-[#232323]"
-								>
-									{item.subheadings?.map((sub) => (
-										<Link
-											key={sub.name}
-											href={sub.href}
-											target={sub.target}
-											className="-m-3 flex items-start rounded-lg p-3"
-										>
-											<ul className="ml-0">
-												<li className="text-base font-medium hover:underline hover:underline-offset-2">
-													{sub.name}
-												</li>
-											</ul>
-										</Link>
-									))}
-								</PopoverContent>
-							</Popover>
-						))}
-					</nav>
-				) : null}
-				<div className="my-auto hidden justify-end pr-7 md:flex md:flex-1">
-					<Link href="/contact" target="_top">
-						<Button variant={"giggles"} className="rounded-full bg-giggles-red">
-							Get in touch
-						</Button>
-					</Link>
-				</div>
-			</div>
-			<Popover modal>
-				<PopoverTrigger className="group px-2 md:hidden">
-					<Icons.menu className="mr-2 h-6 w-6 group-data-[state=open]:hidden group-data-[state=open]:transform" />
-					<Icons.close className="mr-2 hidden h-6 w-6 group-data-[state=open]:flex group-data-[state=open]:transform" />
-				</PopoverTrigger>
-				<PopoverContent className="-mt-1 h-screen overflow-scroll rounded-none border-none">
-					<Accordion
-						type="single"
-						collapsible
-						className="w-screen pr-10 text-2xl"
-					>
-						{items?.map((item, index) => (
-							<AccordionItem
-								value={item.title}
-								key={item.title}
-								className="my-4"
-							>
-								<AccordionTrigger>{item.title}</AccordionTrigger>
-								<AccordionContent>
-									{item.subheadings?.map((sub) => (
-										<Link
-											key={sub.name}
-											href={sub.href}
-											target={sub.target}
-											className="-m-3 my-2 flex items-start rounded-lg p-3 pl-8 text-lg"
-										>
-											{sub.name}
-										</Link>
-									))}
-								</AccordionContent>
-							</AccordionItem>
-						))}
-					</Accordion>
-					<div className="my-auto flex justify-center py-7">
-						<Button className="rounded-full">Get in touch</Button>
-					</div>
-					<div className="mb-[120px] flex h-[20vh] flex-1 items-end justify-center pb-20">
-						{socialMedia.map((social, index) => (
-							<Link key={social.id} href={social.link} target={"_blank"}>
-								<Image
-									src={social.icon}
-									alt={social.id}
-									height={21}
-									width={21}
-									className={`h-[34px] w-[34px] cursor-pointer object-contain hover:scale-105 ${
-										index !== socialMedia.length - 1 ? "mr-6" : "mr-0"
-									}`}
-								/>
-							</Link>
-						))}
-					</div>
-				</PopoverContent>
-			</Popover>
-		</div>
-	);
+                <PopoverContent
+                  side="bottom"
+                  align="start"
+                  alignOffset={-20}
+                  className="shrink text-[#232323]"
+                >
+                  {item.subheadings?.map((sub) => (
+                    <Link
+                      key={sub.name}
+                      href={sub.href}
+                      target={sub.target}
+                      className="-m-3 flex items-start rounded-lg p-3"
+                    >
+                      <ul className="ml-0">
+                        <li className="text-base font-normal hover:underline hover:underline-offset-2">
+                          {sub.name}
+                        </li>
+                      </ul>
+                    </Link>
+                  ))}
+                </PopoverContent>
+              </Popover>
+            ))}
+          </nav>
+        ) : null}
+        <div className="my-auto hidden justify-end pr-7 md:flex md:flex-1">
+          <Link href="/contact" target="_top">
+            <Button variant={"giggles"} className="rounded-full bg-giggles-red">
+              Get in touch
+            </Button>
+          </Link>
+        </div>
+      </div>
+      <Popover modal>
+        <PopoverTrigger className="group px-2 md:hidden">
+          <Icons.menu className="mr-2 h-6 w-6 group-data-[state=open]:hidden group-data-[state=open]:transform" />
+          <Icons.close className="mr-2 hidden h-6 w-6 group-data-[state=open]:flex group-data-[state=open]:transform" />
+        </PopoverTrigger>
+        <PopoverContent className="-mt-1 h-screen overflow-scroll rounded-none border-none">
+          <Accordion
+            type="single"
+            collapsible
+            className="w-screen pr-10 text-2xl"
+          >
+            {items?.map((item, index) => (
+              <AccordionItem
+                value={item.title}
+                key={item.title}
+                className="my-4"
+              >
+                <AccordionTrigger>{item.title}</AccordionTrigger>
+                <AccordionContent>
+                  {item.subheadings?.map((sub) => (
+                    <Link
+                      key={sub.name}
+                      href={sub.href}
+                      target={sub.target}
+                      className="-m-3 my-2 flex items-start rounded-lg p-3 pl-8 text-lg"
+                    >
+                      {sub.name}
+                    </Link>
+                  ))}
+                </AccordionContent>
+              </AccordionItem>
+            ))}
+          </Accordion>
+          <div className="my-auto flex justify-center py-7">
+            <Button className="rounded-full">Get in touch</Button>
+          </div>
+          <div className="mb-[120px] flex h-[20vh] flex-1 items-end justify-center pb-20">
+            {socialMedia.map((social, index) => (
+              <Link key={social.id} href={social.link} target={"_blank"}>
+                <Image
+                  src={social.icon}
+                  alt={social.id}
+                  height={21}
+                  width={21}
+                  className={`h-[34px] w-[34px] cursor-pointer object-contain hover:scale-105 ${
+                    index !== socialMedia.length - 1 ? "mr-6" : "mr-0"
+                  }`}
+                />
+              </Link>
+            ))}
+          </div>
+        </PopoverContent>
+      </Popover>
+    </div>
+  );
 }
--- app/membership/page.tsx
+++ app/membership/page.tsx
@@ -1,57 +1,87 @@
-import Head from "next/head";
+"use client";
 import { Button } from "../components/ui/button";
 import {
-	Tabs,
-	TabsContent,
-	TabsList,
-	TabsTrigger,
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
 } from "@/app/components/ui/tabs";
+import { Carousel } from "@mantine/carousel";
+import Pricing2 from "../components/pricing2";
+import { Icons } from "../components/icons";
+import { discounts } from "../config/site";
+import { CarouselSlide } from "@mantine/carousel/lib/CarouselSlide/CarouselSlide";
 
 const Membership = () => {
-	return (
-		<div className="flex flex-col items-center justify-center p-10">
-			<Head>
-				<title>Membership</title>
-			</Head>
-			<h1 className="mb-5 text-2xl font-bold">Membership</h1>
-			<Tabs defaultValue="monthly" className="w-[400px]">
-				<TabsList>
-					<TabsTrigger value="monthly">Monthly</TabsTrigger>
-					<TabsTrigger value="annual">Annual</TabsTrigger>
-				</TabsList>
-				<TabsContent value="monthly">
-					<h2 className="mb-5 text-xl font-bold">Monthly Memberships</h2>
-					<p className="mb-5 text-gray-700">
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vel
-						ipsum vel velit blandit efficitur. Sed dapibus semper sapien, id
-						consectetur odio consectetur eu.
-					</p>
-					<p className="mb-5 text-gray-700">
-						<strong>Discounts:</strong> Lorem ipsum dolor sit amet, consectetur
-						adipiscing elit. Nam vel ipsum vel velit blandit efficitur.
-					</p>
-				</TabsContent>
-				<TabsContent value="annual">
-					<h2 className="mb-5 text-xl font-bold">Annual Memberships</h2>
-					<p className="mb-5 text-gray-700">
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vel
-						ipsum vel velit blandit efficitur. Sed dapibus semper sapien, id
-						consectetur odio consectetur eu.
-					</p>
-					<p className="mb-5 text-gray-700">
-						<strong>Discounts:</strong> Lorem ipsum dolor sit amet, consectetur
-						adipiscing elit. Nam vel ipsum vel velit blandit efficitur.
-					</p>
-				</TabsContent>
-			</Tabs>
+  return (
+    <div className="flex flex-col items-center justify-center p-10">
+      <h1 className="mb-5 text-2xl font-bold">Membership</h1>
+      <div className="w-full">
+        <Pricing2 />
+      </div>
 
-			{/* Call to Action */}
-			<h2 className="mb-5 text-xl font-bold">Sign Up for Membership</h2>
-			<Button variant={"giggles"} size={"lg"} className="rounded-full" disabled>
-				Sign Up
-			</Button>
-		</div>
-	);
+      {/* Call to Action */}
+      <h2 className="mb-5 text-xl font-bold">Sign Up for Membership</h2>
+      <Button variant={"giggles"} size={"lg"} className="rounded-full" disabled>
+        Sign Up
+      </Button>
+      <Carousel
+        height={600}
+        loop
+        align="start"
+        className="mt-10 w-11/12"
+        withIndicators={false}
+        withControls={false}
+      >
+        {discounts.map((card, index) => (
+          <Carousel.Slide key={index} className="">
+            <div className="mx-auto grid h-fit max-w-[calc(1150px_/_3)] transform rounded-lg px-6 py-4 transition-colors duration-200 hover:bg-gray-200 dark:hover:bg-gray-700">
+              <p className="text-xs font-medium text-gray-400 dark:text-gray-100">
+                {card.description}
+              </p>
+              <p className="text-lg font-medium text-gray-800 dark:text-gray-100">
+                {card.title}
+              </p>
+              <h4 className="mt-2 text-4xl font-semibold text-gray-800 dark:text-gray-100">
+                <span className="text-gray-400 line-through">
+                  {card.crossedOut}
+                </span>
+                {card.price}{" "}
+                <span className="text-base font-normal text-gray-600 dark:text-gray-400">
+                  / {card.per}
+                </span>
+              </h4>
+              <p className="mt-4 text-gray-500 dark:text-gray-300">
+                For most businesses that want to optimaize web queries.
+              </p>
+              <div className="mt-8 space-y-2">
+                {card.features.map((item, index) => (
+                  <div key={index} className="flex items-center">
+                    <Icons.customCheck2 />
+
+                    <span className="mx-4 text-gray-700 dark:text-gray-300">
+                      {item.feature}
+                      <br />
+                      <span className="text-gray-400">{item.caveat}</span>
+                    </span>
+                  </div>
+                ))}
+              </div>
+
+              <Button
+                variant={"giggles"}
+                size={"lg"}
+                className="mt-10 w-full transform rounded-md px-4 py-2 font-medium capitalize tracking-wide"
+              >
+                Choose plan
+              </Button>
+            </div>
+          </Carousel.Slide>
+        ))}
+        {/* ...other slides */}
+      </Carousel>
+    </div>
+  );
 };
 
 export default Membership;
